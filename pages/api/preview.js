924f57fbf53c5244da7403237fde8ed9d3f2db45
Joe Al-mendhry <joe.al-mendhry@cef.co.uky>
2021-03-10 16:33:50 +0000

    preview

--- pages/api/preview.js
+++ pages/api/preview.js
@@ -9,7 +9,7 @@ export default async function preview(req, res) {
 
   // Fetch the headless CMS to check if the provided `slug` exists
   const post = await getPageEntryBySlug(slug, true);
-
+  console.log(post);
   // If the slug doesn't exist prevent preview mode from being enabled
   if (!post) {
     return res.status(401).json({ message: 'Invalid slug' });
