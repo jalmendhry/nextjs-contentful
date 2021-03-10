ab1d4f60caa6b5c6b57c92845b40b50175f25d28
Joe Al-mendhry <joe.al-mendhry@cef.co.uky>
2021-03-10 11:24:56 +0000

    test env var

--- pages/[page].js
+++ pages/[page].js
@@ -5,6 +5,7 @@ import matter from 'gray-matter';
 import ReactMarkdown from 'react-markdown';
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import Image from 'next/image';
 
 import Layout from '../components/Layout';
 
@@ -15,6 +16,7 @@ const Page = ({ pageData: { title, carousel, contentRow, content } }) => {
         {carousel && carousel.length > 0 && (
           <Carousel>
             {carousel.map((image, index) => {
+              // console.log(carousel);
               return (
                 <div key={index}>
                   <img src={image} alt=""></img>
@@ -34,7 +36,7 @@ const Page = ({ pageData: { title, carousel, contentRow, content } }) => {
         {contentRow &&
           contentRow.map((row, index) => {
             const { content, image } = row;
-
+            console.log(image);
             return (
               <div className="content-row" key={index}>
                 <img src={image} alt="" />
@@ -50,6 +52,7 @@ const Page = ({ pageData: { title, carousel, contentRow, content } }) => {
 };
 
 export const getStaticPaths = async () => {
+  console.log(process.env.CONTENTFUL_SPACE_ENV);
   const files = fs.readdirSync('cms/pages/');
   const paths = files.map((filename) => ({
     params: {
@@ -64,6 +67,7 @@ export const getStaticPaths = async () => {
 };
 
 export const getStaticProps = async ({ params: { page } }) => {
+  console.log(process.env.CONTENTFUL_SPACE_ENV);
   const markdownWithMetadata = fs
     .readFileSync(path.join('cms/pages', page + '.md'))
     .toString();
